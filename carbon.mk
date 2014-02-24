b0bd01f453f7c958a1e6aa9c4d6931beff013a00
John Kirk <jkirk777@gmail.com>
2014-02-23 19:36:53 -0700

    Make it carbon's in an attempt to fool gerrit

--- carbon.mk
+++ carbon.mk
@@ -1,10 +1,7 @@
 $(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2mtr \
@@ -12,6 +9,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMF2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2mtr-user 4.1.2 JZO54K R530MVQAMF2 release-keys"
 
-PRODUCT_NAME := cm_d2mtr
+PRODUCT_NAME := carbon_d2mtr
 PRODUCT_DEVICE := d2mtr
 
